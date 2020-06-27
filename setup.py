e96dba553b88c09235599be34ab342de8c1f6623
ogogmad <jkabrg@gmail.com>
2020-06-27 15:58:01 +0100

    Make new release (Matplotlib as default library for displaying animations)

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from distutils.core import setup
 setup(
   name = 'laguerre_transformations',
   packages = ['laguerre_transformations'],
-  version = '0.23',
+  version = '0.3',
   license='BSD-3-Clause',
   description = 'Visualize Laguerre transformations.',
   author = 'Ran Gutin',
   author_email = 'jkabrg@gmail.com',
   url = 'https://github.com/ogogmad/laguerre_transformations',
-  download_url = 'https://github.com/ogogmad/laguerre_transformations/archive/v_023.tar.gz',
+  download_url = 'https://github.com/ogogmad/laguerre_transformations/archive/v_03.tar.gz',
   keywords = ['mathematics', 'transformations', 'geometry', 'hypercomplex'],
   install_requires=[
           'scipy',
           'Pillow',
+          'matplotlib'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',
